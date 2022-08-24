25dbefad56e7145aec9484e7ea1f29ae73b58c31
Erik Nelsestuen <erik.nelsestuen@gmail.com>
2022-08-24 13:09:24 -0600

    Update ulbow.rb

--- Casks/ulbow.rb
+++ Casks/ulbow.rb
@@ -1,9 +1,9 @@
 cask 'ulbow' do
-  version '1.5.1'
-  sha256 'd283a3cc2dc62bc8a15388cf1a899cbd3892b0c79eecd3428637a2110007a40b'
+  version '1.7'
+  sha256 'b4770da1bfb00833ab78be6c2fdde41f86e89233f75c6f3f3b22457fee231798'
 
   # eclecticlightdotcom.files.wordpress.com was verified as official when first introduced to the cask
-  url "https://github.com/tmoreno/open-log-viewer/releases/download/#{version}/open-log-viewer_#{version}/#{version}.dmg"
+  url "https://eclecticlightdotcom.files.wordpress.com/2021/04/ulbow17.ziphttps://github.com/tmoreno/open-log-viewer/releases/download/#{version}/open-log-viewer_#{version}/#{version}.dmg"
   name 'ulbow'
   homepage 'https://eclecticlight.co/2020/06/30/finding-your-way-around-the-log-using-ulbow/'
 
